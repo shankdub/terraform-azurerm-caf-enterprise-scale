74de2c92850684751e83215d2918c487fd87a806
Rob Gonzalez <robertgonzalez@shankdubmusic.onmicrosoft.com>
2024-06-11 15:25:11 -0600

    chore: Update azurerm provider configuration and add connectivity provider

--- modules/connectivity/main.tf
+++ modules/connectivity/main.tf
@@ -1,2 +1,7 @@
 # No resources deployed by this module so this file is here as an entry point only
 # Please navigate the variables, locals and outputs to see how the data model is generated from the inputs
+provider "azurerm" {
+  alias           = "connectivity"
+  subscription_id = "5f7553d2-1a6e-46df-89fd-4b5fe00fef96"
+  features {}
+}
