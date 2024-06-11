9e986b5eaad7b511b2fa5667c373bb7ed104912b
Rob Gonzalez <robertgonzalez@shankdubmusic.onmicrosoft.com>
2024-06-11 08:35:11 -0600

    chore: Update createAzSubs.sh script

--- createAzSubs.sh
+++ createAzSubs.sh
@@ -1,13 +1,12 @@
 az config set extension.use_dynamic_install=yes_without_prompt
 az extension add --name account 
 az extension add --name alias
-az extension add --name account
 #get the billing account
 billingAccountJson=$(az billing account list --query "[0]")
 billingAccountName=$(echo $billingAccountJson | jq -r '.name')
 billingProfileJson=$(az billing profile list --account-name $billingAccountName --expand "InvoiceSections" --query "[0]")
 billingScope=$(echo $billingProfileJson | jq -r '.invoiceSections.value[0].id')
-# Create a new subscription alias
+#create  new subscription(s)
 subnames=("management" "security")
 for subname in "${subnames[@]}"; do
     az account alias create --name "$subname" --billing-scope "$billingScope" --display-name "$subname" --workload "Production"
