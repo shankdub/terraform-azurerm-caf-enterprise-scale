a364dcf269d67b56e5a2610e0578df16a03920b5
shankdub <49691426+shankdub@users.noreply.github.com>
2024-06-10 20:46:35 -0600

    azuresub creation shell script

---
+++ createAzSubs.sh
@@ -0,0 +1,18 @@
+az config set extension.use_dynamic_install=yes_without_prompt
+az extension add --name account 
+az extension add --name alias
+az extension add --name account
+#get the billing account
+billingAccountJson=$(az billing account list --query "[0]")
+billingAccountName=$(echo $billingAccountJson | jq -r '.name')
+billingProfileJson=$(az billing profile list --account-name $billingAccountName --expand "InvoiceSections" --query "[0]")
+billingScope=$(echo $billingProfileJson | jq -r '.invoiceSections.value[0].id')
+# Create a new subscription alias
+subnames=("management" "security")
+for subname in "${subnames[@]}"; do
+    az account alias create --name "$subname" --billing-scope "$billingScope" --display-name "$subname" --workload "Production"
+    sleep 60
+    subId=$(az account show --subscription "$subname" --query 'id' -o tsv)
+    echo "Subscription $subname has been created with ID $subId"
+done
+
